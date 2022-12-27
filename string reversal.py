83bf79ac5190234bdfb75abc43d4745c684d090b
Navin Tamilan <navintamilan31@gmail.com>
2022-12-27 20:29:37 +0530

    String Reversal

---
+++ string reversal.py
@@ -0,0 +1,4 @@
+n=str(input("Please enter any word to get reversal : "))
+print("Before Reversal : ",n)
+print("After Reversal : ", n[-1::-1]) #Here we used slice_array concept i.e arr[start_value : Stop-1 value : step]
+
