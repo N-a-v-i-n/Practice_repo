ae3f02a098676fe6ff8cc2c6fa65abc395d03ed3
Navin Tamilan <navintamilan31@gmail.com>
2022-12-29 05:38:14 +0530

    Print_function

---
+++ print_function.py
@@ -0,0 +1,9 @@
+"""
+This code is use to print n's value in sequence manner, and not space b/w them
+ex:- n=3
+     o/p :- 123
+
+"""
+n=int(input("enter any number"))
+for x in range(1,n+1):
+    print(x,end="")
